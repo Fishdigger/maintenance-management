271d367bea2e3985aa270ea81c8612686eb971cf
Joel Gregory <joelgregory7@gmail.com>
2017-05-13 18:42:51 -0400

    Data validation

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,2 +1,6 @@
 class System < ApplicationRecord
+  validates :name, presence: true,
+    length: { minimum: 5}
+  validates :part_number, presence: true
+  validates :serial_number, presence: true
 end
