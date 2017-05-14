9754e9d8aa979b90716ea4993c9d984c8524f509
Joel Gregory <joelgregory7@gmail.com>
2017-05-13 21:45:37 -0400

    Changed name minimum length

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,6 +1,6 @@
 class System < ApplicationRecord
   validates :name, presence: true,
-    length: { minimum: 5}
+    length: { minimum: 3}
   validates :part_number, presence: true
   validates :serial_number, presence: true
 end
