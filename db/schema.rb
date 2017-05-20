4e56658693dee9932d1a3e44faba7b7b83e83e27
Joel Gregory <joelgregory7@gmail.com>
2017-05-20 16:52:08 -0400

    Add location model, view, and controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520004207) do
+ActiveRecord::Schema.define(version: 20170520204645) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "street"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170520004207) do
     t.string   "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "nickname"
   end
 
   create_table "parts", force: :cascade do |t|
